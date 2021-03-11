ebe04e1ec03fc89670cc612d239fb2f258bad5c0
hero-BA <suad.hero@gmail.com>
2021-03-11 22:01:25 +0100

    Added exercise: Declare string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+  // Only change code below this line
+  var myFirstName = "Suad";
+  var myLastName = "Hero";
+  var myHomeTown = "Sarajevo";
+  // Only change code above this line
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+module.exports = main;
