e803e34e6a62598a812a30e3422760c107078ae9
hero-BA <suad.hero@gmail.com>
2021-03-11 23:44:40 +0100

    Modified exercise 01

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,22 +1,18 @@
-function main(numOne, numTwo){
+function main(numOne, numTwo) {
+  var quotient;
+  if (numTwo === 0) {
+    quotient = "Divide by 0! Cannot divide by zero!";
+  } else {
+    // Only change code below this line
+    quotient = numOne / numTwo;
+    // Only change code above this line
+  }
 
-    var quotient;
-    if(numTwo === 0){
-        quotient = "Divide by 0! Cannot divide by zero!";
-    } else {
-        // Only change code below this line
-        var numOne;
-        var numTwo;
-        
-        quotient = numOne / numTwo;
-        // Only change code above this line
-    }
-
-    return quotient;
+  return quotient;
 }
 
-console.log(main(15, 3));    
+console.log(main(15, 3));
 console.log(main(25, 5));
 console.log(main(420, 10));
-console.log(main(55, 0));     
+console.log(main(55, 0));
 module.exports = main;
